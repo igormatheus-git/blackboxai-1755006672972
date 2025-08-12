b6c08e33bb98655561fbff84afc6d4a4157c0088
igormatheus-git <higormatheus09871@gmail.com>
2025-08-12 10:51:49 -0300

    Add .next/static/chunks/src_app_social-simulator_page_tsx_a993527f._.js 1755006707978

---
+++ .next/static/chunks/src_app_social-simulator_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_f5a63f7e._.js",
+  "static/chunks/node_modules_0107fb58._.js"
+],
+    source: "dynamic"
+});
