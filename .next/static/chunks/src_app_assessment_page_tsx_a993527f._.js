c941fe57bd6fee9498a238c2a7307628668edf4d
igormatheus-git <higormatheus09871@gmail.com>
2025-08-12 10:52:07 -0300

    Add .next/static/chunks/src_app_assessment_page_tsx_a993527f._.js 1755006726043

---
+++ .next/static/chunks/src_app_assessment_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_476dce5b._.js",
+  "static/chunks/node_modules_5a2ef644._.js"
+],
+    source: "dynamic"
+});
